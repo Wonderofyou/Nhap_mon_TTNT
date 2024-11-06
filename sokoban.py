bf66cbf0c36267752bca6287b7e7eacbb6d61d93
nguyenconglong <nguyenconglong115@gmail.com>
2024-11-06 20:46:47 +0700

    statics and pause button

--- sokoban.py
+++ sokoban.py
@@ -29,8 +29,34 @@ def draw_weight_on_box(number, x, y, image):
 
     # Vẽ số lên hình tròn
     screen.blit(text_surface, text_rect)
+def draw_pause_btn(screen, on_pause):
+    button_x, button_y, button_width, button_height = 1000, 500, 180, 40
+    radius = 5
+    border_thickness = 2
+    font = pygame.font.SysFont('Verdana', 14, bold=True)
+    button_text = font.render("Pause/Continue", True, (0, 0, 0))
+    button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
+    button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
 
-def print_game(matrix,screen, step, total_weight, boxes=None):
+    
+    if on_pause:
+      color =  (200, 200, 200)
+    else:
+      color = (150, 150, 150)
+
+    # Vẽ viền
+    pygame.draw.rect(screen, (0, 0, 0), button_rect, border_radius=radius)
+
+    # Vẽ button bên trong
+    inner_rect = button_rect.inflate(-border_thickness*2, -border_thickness*2)
+    pygame.draw.rect(screen, color, inner_rect, border_radius=radius)
+
+    # Vẽ văn bản
+    screen.blit(button_text, (button_x + (button_width - button_text.get_width()) // 2,
+                              button_y + (button_height - button_text.get_height()) // 2))
+    return button_x, button_y, button_width, button_height
+   
+def print_game(matrix,screen, step, total_weight, on_pause, boxes=None):
     positions = [x[:2] for x in boxes]  # Lấy 2 phần tử đầu của mỗi phần tử trong a
     weights = [x[-1] for x in boxes]
     screen.fill(background)
@@ -38,7 +64,8 @@ def print_game(matrix,screen, step, total_weight, boxes=None):
     y = 0
     font = pygame.font.Font(None, 36)
     steps_text = font.render(f"Steps: {step}", True, (0, 0, 0))
-    weight_text = font.render(f"Power: {total_weight}", True, (0, 0, 0))
+    weight_text = font.render(f"Weight: {total_weight}", True, (0, 0, 0))
+    draw_pause_btn(screen, on_pause)
     
     screen.blit(steps_text, (900, 50))
     screen.blit(weight_text, (900, 100))
@@ -150,7 +177,7 @@ pygame.init()
 moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 def choose_algo(screen, btns):
     w = 1055
-    h1 = 200
+    h1 = 100
     x0 = 130
     y0 = 40
     waiting_option = True
@@ -166,14 +193,14 @@ def choose_algo(screen, btns):
                    if y >= h1 + 100*i and y <= h1 + 100*i + y0:
                       return options[i]
              
-        print_game(_game.start_state.get_matrix(), screen, 0, 0, _game.start_state.box)
+        print_game(_game.start_state.get_matrix(), screen, 0, 0, 0, _game.start_state.box)
         for btn in btns:
             btn.draw()
         pygame.display.flip()
     return 'BFS'
 def rerender_running(screen, message_box, btns):
   while not stop_event.is_set():
-    print_game(_game.start_state.get_matrix(), screen, 0, 0, _game.start_state.box)
+    print_game(_game.start_state.get_matrix(), screen, 0, 0, 0, _game.start_state.box)
     for btn in btns:
        btn.draw()
     display_box(screen, message_box)
@@ -203,7 +230,7 @@ while True:
     stop_event.set()
     thread_render.join()
     
-    print_game(_game.start_state.get_matrix(), screen, 0, 0, _game.start_state.box)
+    print_game(_game.start_state.get_matrix(), screen, 0, 0, 0, _game.start_state.box)
     
     pygame.display.update()
 
@@ -212,7 +239,15 @@ while True:
     index = 0 
     is_drawn = True  # Khởi tạo với True để bắt đầu di chuyển đầu tiên
     total_weight = 0
+    on_pause = False
     while True:
+        for event in pygame.event.get():
+           if event.type == pygame.MOUSEBUTTONDOWN:
+              if pygame.Rect(1000, 500, 180, 40).collidepoint(event.pos):
+                 on_pause = not on_pause
+        if on_pause:
+           print_game(_game.start_state.get_matrix(), screen, index, total_weight, on_pause, _game.start_state.box)
+           continue
         total_weight += flag[index]
         
         # Chỉ thực hiện di chuyển nếu lần cập nhật màn hình trước đó đã hoàn tất
@@ -223,7 +258,7 @@ while True:
             is_drawn = False  # Đặt lại flag, chờ việc vẽ hoàn tất
 
         # Cập nhật màn hình
-        print_game(_game.start_state.get_matrix(), screen, index, total_weight, _game.start_state.box)
+        print_game(_game.start_state.get_matrix(), screen, index, total_weight, on_pause, _game.start_state.box)
         pygame.display.update()
         is_drawn = True  # Đặt lại flag sau khi cập nhật xong màn hình
 
@@ -231,7 +266,7 @@ while True:
         if _game.start_state.is_completed():
             pygame.display.update()
             display_end(screen=screen)
-            pygame.time.delay(10000)  # Đợi một lúc trước khi quay lại màn hình chọn
+            pygame.time.delay(5000)  # Đợi một lúc trước khi quay lại màn hình chọn
             break  # Quay lại vòng lặp bên ngoài để chọn level mới
 
-        pygame.time.delay(50)
+        pygame.time.delay(800)
--- widgets.py
+++ widgets.py
@@ -2,25 +2,25 @@ from events import *
 from pygame_widgets.button import Button
 def sidebar_widgets(window):
     astar_button = Button(
-		window, 1055, 200, 130, 40, text='A*', radius=5,
+		window, 1055, 100, 130, 40, text='A*', radius=5,
 		font=pygame.font.SysFont('Verdana', 14, bold=True),
 		onClick=lambda: pygame.event.post(pygame.event.Event(SOLVE_ASTAR_EVENT)),
 		borderColor='black', borderThickness=2,
 	)
     bfs_button = Button(
-		window, 1055, 300, 130, 40, text='BFS', radius=5,
+		window, 1055, 200, 130, 40, text='BFS', radius=5,
 		font=pygame.font.SysFont('Verdana', 14, bold=True),
 		onClick=lambda: pygame.event.post(pygame.event.Event(SOLVE_BFS_EVENT)),
 		borderColor='black', borderThickness=2,
 	)
     dfs_button = Button(
-		window, 1055, 400, 130, 40, text='DFS', radius=5,
+		window, 1055, 300, 130, 40, text='DFS', radius=5,
 		font=pygame.font.SysFont('Verdana', 14, bold=True),
 		onClick=lambda: pygame.event.post(pygame.event.Event(SOLVE_DFS_EVENT)),
 		borderColor='black', borderThickness=2,
 	)
     ucs_button = Button(
-		window, 1055, 500, 130, 40, text='UCS', radius=5,
+		window, 1055, 400, 130, 40, text='UCS', radius=5,
 		font=pygame.font.SysFont('Verdana', 14, bold=True),
 		onClick=lambda: pygame.event.post(pygame.event.Event(SOLVE_UCS_EVENT)),
 		borderColor='black', borderThickness=2,
